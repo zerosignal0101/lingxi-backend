c184e336a75e1b6f3418364df52d5b37720915f2
丁德健 <wddlzzxsdz@outlook.com>
2024-05-11 07:41:50 +0800

    1

--- tool/db.js
+++ tool/db.js
@@ -1,16 +1,9 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: '116.62.197.58',
+    host: '127.0.0.1',
     user: 'root',
-    password: 'Bk1770!Dev@',
+    password: '123456',
     database: 'MakingFriends'
 });
-
-//var connection = mysql.createConnection({
-//    host: '127.0.0.1',
-//    user: 'root',
-//    password: '123456',
-//    database: 'MakingFriends'
-//});
 module.exports = connection;
